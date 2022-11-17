54caae234cb450e7c24954cd4418abf01a49634b
Viral <viralpatel742001@gmail.com>
2022-11-17 21:25:06 +0530

    Make validation in one function

--- src/componants/editUser.js
+++ src/componants/editUser.js
@@ -1,57 +1,92 @@
-import React,{useContext,useState,useEffect} from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { UserListData } from './Userdata'
 import { useParams } from 'react-router-dom'
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 function EditUser() {
-    const {edituser,users} = useContext(UserListData)
-    const {id} = useParams()
+    const { edituser, users } = useContext(UserListData)
+    const { id } = useParams()
     const nav = useNavigate()
-    const [isErr,setIsErr] = useState(false)
-    const [suser,setSuser] = useState({
-        name:'',
-        email:'',
-        id:''
+    const [isErr, setIsErr] = useState(false)
+    const [suser, setSuser] = useState({
+        name: '',
+        email: '',
+        id: ''
     })
-    const [error,setError] = useState({
-        name:'',
-        email:''
+    const [error, setError] = useState({
+        name: '',
+        email: ''
     })
 
-    const changeemail = (e) => {
-        if(e.target.value === ""){
-            setError({...error,email:'enter email'})
-            setSuser({
-                ...suser,email:e.target.value
-            })
-        }else{
-            setError({...error,email:""})
-        setSuser({
-            ...suser,email:e.target.value
-        })}
-    }
-    const changename = (e) => {
-        if(e.target.value === ""){
-            setError({...error,name:'enter name'})
-            setSuser({
-                ...suser,name:e.target.value
-            })
-        }else{
-        setError({...error,name:''})
-        setSuser({
-            ...suser,name:e.target.value
-        })}
+    // const changeemail = (e) => {
+    //     if(e.target.value === ""){
+    //         setError({...error,email:'enter email'})
+    //         setSuser({
+    //             ...suser,email:e.target.value
+    //         })
+    //     }else{
+    //         setError({...error,email:""})
+    //     setSuser({
+    //         ...suser,email:e.target.value
+    //     })}
+    // }
+    // const changename = (e) => {
+    //     if(e.target.value === ""){
+    //         setError({...error,name:'enter name'})
+    //         setSuser({
+    //             ...suser,name:e.target.value
+    //         })
+    //     }else{
+    //     setError({...error,name:''})
+    //     setSuser({
+    //         ...suser,name:e.target.value
+    //     })}
+    // }
+    
+    const changeData = (e) => {
+        switch (e.target.name) {
+            case "email":
+                if (e.target.value === "") {
+                    setError({ ...error, email: 'enter email' })
+                    setSuser({
+                        ...suser, email: e.target.value
+                    })
+                } else {
+                    setError({ ...error, email: "" })
+                    setSuser({
+                        ...suser, email: e.target.value
+                    })
+                }
+
+                break
+            case "name":
+                if (e.target.value === "") {
+                    setError({ ...error, name: 'enter name' })
+                    setSuser({
+                        ...suser, name: e.target.value
+                    })
+                } else {
+                    setError({ ...error, name: '' })
+                    setSuser({
+                        ...suser, name: e.target.value
+                    })
+                }
+
+                break
+            default:
+                break
+        }
     }
     const editthisuser = () => {
-        
-        if(error.name !== "" || error.email !== ""){
+
+        if (error.name !== "" || error.email !== "") {
             setIsErr(true)
-            return 
+            return
         }
-        if(!suser.email.includes("@")){
+        if (!suser.email.includes("@")) {
             setError({
-                ...error,email:'invalid email'
+                ...error, email: 'invalid email'
             })
             setIsErr(true)
             return
@@ -59,19 +94,19 @@ function EditUser() {
         edituser(suser)
         nav('/')
     }
-    useEffect( () => {
-        const thisuser = users.find(user=>user.id == id)
+    useEffect(() => {
+        const thisuser = users.find(user => user.id == id)
         setSuser(thisuser)
-    },[])
-  return (
-    <div>
-        <input type="text" onChange={changeemail} value={suser.email} />
-        {isErr&&<span>{error.email}</span>}
-        <input type="text" onChange={changename} value={suser.name} />
-        {isErr&&<span>{error.name}</span>}
-        <button onClick={editthisuser} >edit</button>
-    </div>
-  )
+    }, [])
+    return (
+        <div>
+            <input type="text" name='email' onChange={(e) => changeData(e)} value={suser.email} />
+            {isErr && <span>{error.email}</span>}
+            <input type="text" name='name' onChange={(e) => changeData(e)} value={suser.name} />
+            {isErr && <span>{error.name}</span>}
+            <button onClick={editthisuser} >edit</button>
+        </div>
+    )
 }
 
 export default EditUser
